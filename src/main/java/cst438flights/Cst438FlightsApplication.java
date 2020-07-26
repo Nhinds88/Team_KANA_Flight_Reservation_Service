00795db925d1776055a85bc3e9b27e61a2ba8cc6
Andrew T <48335990+espresso64@users.noreply.github.com>
2020-07-26 09:47:13 -0700

    Base starter files for project

---
+++ src/main/java/cst438flights/Cst438FlightsApplication.java
@@ -0,0 +1,13 @@
+package cst438flights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438FlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438FlightsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cst438flights/Cst438FlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438flights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438FlightsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
